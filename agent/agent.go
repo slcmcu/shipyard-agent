dd9f5e5b4ca16a4b84f5b285a9d9a5401ff6423f
Evan Hazlett <ejhazlett@gmail.com>
2014-02-14 23:13:51 -0500

    use docker socket

--- agent/agent.go
+++ agent/agent.go
@@ -6,6 +6,8 @@ import (
 	"flag"
 	"fmt"
 	"github.com/dotcloud/docker"
+	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
@@ -18,15 +20,16 @@ import (
 	"time"
 )
 
-const VERSION string = "0.1.0"
+const VERSION string = "0.1.1"
 
 var (
-	dockerURL     string
+	dockerPath    string
 	shipyardURL   string
 	shipyardKey   string
 	runInterval   int
 	registerAgent bool
 	version       bool
+	address       string
 	port          int
 )
 
@@ -35,8 +38,25 @@ type (
 		Key string `json:"key"`
 	}
 
+	Port struct {
+		IP          string
+		PrivatePort int
+		PublicPort  int
+		Type        string
+	}
+
+	APIContainer struct {
+		Id      string
+		Created int
+		Image   string
+		Status  string
+		Command string
+		Ports   []Port
+		Names   []string
+	}
+
 	ContainerData struct {
-		Container docker.APIContainers
+		Container APIContainer
 		Meta      *docker.Container
 	}
 
@@ -55,12 +75,13 @@ type (
 )
 
 func init() {
-        flag.StringVar(&dockerURL, "docker", "http://127.0.0.1:4243", "URL to Docker")
+	flag.StringVar(&dockerPath, "docker", "/var/run/docker.sock", "Path to Docker socket")
 	flag.StringVar(&shipyardURL, "url", "", "Shipyard URL")
 	flag.StringVar(&shipyardKey, "key", "", "Shipyard Agent Key")
 	flag.IntVar(&runInterval, "interval", 5, "Run interval")
 	flag.BoolVar(&registerAgent, "register", false, "Register Agent with Shipyard")
 	flag.BoolVar(&version, "version", false, "Shows Agent Version")
+	flag.StringVar(&address, "address", "", "Listen address (default: 0.0.0.0)")
 	flag.IntVar(&port, "port", 4500, "Agent Listen Port")
 
 	flag.Parse()
@@ -69,6 +90,14 @@ func init() {
 		fmt.Println(VERSION)
 		os.Exit(0)
 	}
+}
+
+func newDockerClient() (*httputil.ClientConn, error) {
+	conn, err := net.Dial("unix", dockerPath)
+	if err != nil {
+		return nil, err
+	}
+	return httputil.NewClientConn(conn, nil), nil
 }
 
 func updater(jobs <-chan *Job, group *sync.WaitGroup) {
@@ -99,14 +128,23 @@ func updater(jobs <-chan *Job, group *sync.WaitGroup) {
 	}
 }
 
-func getContainers() []*docker.APIContainers {
-	path := fmt.Sprintf("%s/containers/json?all=1", dockerURL)
-	resp, err := http.Get(path)
-	defer resp.Body.Close()
+func getContainers() []APIContainer {
+	path := "/containers/json?all=1"
+	c, err := newDockerClient()
+	defer c.Close()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error requesting containers from Docker: %s", err)
 	}
-	var containers []*docker.APIContainers
+	req, err := http.NewRequest("GET", path, nil)
+	if err != nil {
+		log.Fatalf("Error requesting containers from Docker: %s", err)
+	}
+	resp, err := c.Do(req)
+	if err != nil {
+		log.Fatalf("Error requesting containers from Docker: %s", err)
+	}
+	defer resp.Body.Close()
+	var containers []APIContainer
 	if resp.StatusCode == http.StatusOK {
 		d := json.NewDecoder(resp.Body)
 		if err = d.Decode(&containers); err != nil {
@@ -117,8 +155,20 @@ func getContainers() []*docker.APIContainers {
 }
 
 func inspectContainer(id string) *docker.Container {
-	path := fmt.Sprintf("%s/containers/%s/json?all=1", dockerURL, id)
-	resp, err := http.Get(path)
+	path := fmt.Sprintf("/containers/%s/json?all=1", id)
+	c, err := newDockerClient()
+	defer c.Close()
+	if err != nil {
+		log.Fatalf("Error requesting containers from Docker: %s", err)
+	}
+	req, err := http.NewRequest("GET", path, nil)
+	if err != nil {
+		log.Fatalf("Error requesting containers from Docker: %s", err)
+	}
+	resp, err := c.Do(req)
+	if err != nil {
+		log.Fatalf("Error requesting containers from Docker: %s", err)
+	}
 	defer resp.Body.Close()
 	if err != nil {
 		log.Fatal(err)
@@ -134,7 +184,7 @@ func inspectContainer(id string) *docker.Container {
 }
 
 func getImages() []*Image {
-	path := fmt.Sprintf("%s/images/json?all=0", dockerURL)
+	path := fmt.Sprintf("%s/images/json?all=0", dockerPath)
 	resp, err := http.Get(path)
 	defer resp.Body.Close()
 	if err != nil {
@@ -156,8 +206,8 @@ func pushContainers(jobs chan *Job, group *sync.WaitGroup) {
 	containers := getContainers()
 	data := make([]ContainerData, len(containers))
 	for x, c := range containers {
-		i := inspectContainer(c.ID)
-		containerData := ContainerData{Container: *c, Meta: i}
+		i := inspectContainer(c.Id)
+		containerData := ContainerData{Container: c, Meta: i}
 		data[x] = containerData
 	}
 
@@ -190,7 +240,7 @@ func listen(d time.Duration) {
 
 	for _ = range time.Tick(d) {
 		go pushContainers(jobs, pushGroup)
-		go pushImages(jobs, pushGroup)
+		//go pushImages(jobs, pushGroup)
 		pushGroup.Wait()
 	}
 
@@ -248,7 +298,101 @@ func register() string {
 		log.Fatal(err)
 	}
 	log.Println("Agent Key: ", data.Key)
-        return data.Key
+	return data.Key
+}
+
+func dockerHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("Docker Request: %s", req.URL.Path)
+	client, e := newDockerClient()
+	path := req.URL.Path
+	if strings.Index(path, "attach") != -1 {
+		path = fmt.Sprintf("%s?logs=1&stream=0&stdout=1", path)
+	}
+	if e != nil {
+		log.Printf("Error requesting %s from Docker: %s", path, e)
+		return
+	}
+	req, err := http.NewRequest(req.Method, path, req.Body)
+	if err != nil {
+		log.Printf("Error requesting %s from Docker: %s", path, err)
+		return
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("Error requesting %s from Docker: %s", path, err)
+		return
+	}
+	w.WriteHeader(resp.StatusCode)
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error parsing response from Docker: %s", err)
+		return
+	}
+	w.Write([]byte(content))
+}
+
+type UnixHandler struct {
+	path string
+}
+
+func (h *UnixHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	conn, err := net.Dial("unix", h.path)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Error connecting to unix socket: %s", err)
+		return
+	}
+	c := httputil.NewClientConn(conn, nil)
+	defer c.Close()
+
+	res, err := c.Do(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Error client.Do: %s", err)
+		return
+	}
+	defer res.Body.Close()
+
+	copyHeader(w.Header(), res.Header)
+	if _, err := io.Copy(w, res.Body); err != nil {
+		log.Printf("Error io.Copy body: %s", err)
+	}
+}
+
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
+}
+
+func createTcpHandler(e string) http.Handler {
+	u, err := url.Parse(e)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return httputil.NewSingleHostReverseProxy(u)
+}
+
+func createUnixHandler(e string) http.Handler {
+	return &UnixHandler{e}
+}
+
+func createHandler(e string) http.Handler {
+	var (
+		mux = http.NewServeMux()
+		h   http.Handler
+	)
+
+	if strings.Contains(e, "http") {
+		h = createTcpHandler(e)
+	} else {
+		h = createUnixHandler(e)
+	}
+
+	mux.Handle("/", h)
+	return mux
 }
 
 func main() {
@@ -266,27 +410,10 @@ func main() {
 		shipyardKey = register()
 	}
 
-
 	log.Printf("Shipyard Agent (%s)\n", shipyardURL)
-	u, err := url.Parse(dockerURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var (
-		proxy    = httputil.NewSingleHostReverseProxy(u)
-		director = proxy.Director
-	)
-
-	proxy.Director = func(req *http.Request) {
-		src := strings.Split(req.RemoteAddr, ":")[0]
-		log.Printf("Request from %s: %s\n", src, req.URL.Path)
-		director(req)
-	}
 
 	go listen(duration)
 
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), proxy); err != nil {
-		log.Fatal(err)
-	}
+	handler := createHandler(dockerPath)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", address, port), handler))
 }
